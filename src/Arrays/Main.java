b377cc75959f79c6e60ace627b4c34b0f958e841
Abhishek Gaurav <abhishekgaurav@Abhisheks-MacBook-Air.local>
2024-09-23 09:57:14 +0530

    added code for array

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -2,10 +2,17 @@ package Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        MyArray arr = new MyArray(5);
+   public static void main(String[] args) {
+        MyArray arr = new MyArray(9);
 
         arr.insert(1);
         arr.insert(2);
+        arr.insert(3);
+        arr.insert(4);
+        arr.insert(5);
+
+      //  arr. removeAt(2);
+
+        System.out.println(arr.min());
     }
 }
--- src/Arrays/MyArray.java
+++ src/Arrays/MyArray.java
@@ -14,4 +14,51 @@ public class MyArray {
         this.items[currentIndex]=value;
         currentIndex++;
     }
+
+    public int indexOf(int value){
+        // Linear Search
+        for(int i=0; i < this.currentIndex; i++){
+            if(this.items[i] == value)
+                return i;
+        }
+        return -1;
+    }
+
+    public void removeAt(int index){
+        if(index >= this.currentIndex) throw new IllegalArgumentException();
+        for(int i = index; i <= this.currentIndex-2; i++){
+            this.items[i] = this.items[i+1];
+
+            currentIndex--;
+            this.items[currentIndex] = 0;
+        }
+    }
+
+    public int max(){
+        int result = this.items[0];
+        for(int i=0; i<this.currentIndex; i++){
+            if(items[i] > result)
+                result = items[i];
+        }
+        return result;
+    }
+
+    public int min(){
+        int result = this.items[0];
+        for(int i=0; i<this.currentIndex; i++){
+            if(items[i] < result)
+                result=items[i];
+        }
+        return result;
+    }
+
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        str.append("[");
+        for(int i=0; i<this.currentIndex; i++){
+            str.append(this.items[i]).append(", ");
+        }
+        str.append("\b").append("\b").append("]");
+        return str.toString();
+    }
 }
