77043df0395c6afc3bc030c34f65bb0a56fae186
Abhishek Gaurav <gauravabhishek400@gmail.com>
2025-10-10 17:54:04 +0530

    added code

---
+++ src/AllDivisors.java
@@ -0,0 +1,43 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AllDivisors {
+
+    public static void printAllDivisors(int num){
+
+        ArrayList<Integer> ansList = new ArrayList<>();
+
+        // BRUTE FORCE :: Iterate from 1 to N and check if its divide the num completely
+        // TC : O(N)
+//        for(int i=1; i<=num; i++){
+//            if(num % i == 0){
+//                ansList.add(i);
+//            }
+//        }
+//
+        // OPTIMISED APPROACH
+        // Loop till square root of the number and check the division.
+        // TC : O(sqrt(N))
+
+        for(int i=1; i<= Math.sqrt(num); i++){
+            if(num % i == 0){
+                ansList.add(i);
+                // condition to avoid duplication of elements
+                if(num/i != i){
+                    ansList.add(num/i);
+                }
+            }
+        }
+        ansList.sort(null);
+        ansList.stream().forEach(item -> System.out.println(item));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number = ");
+        int num = sc.nextInt();
+        printAllDivisors(num);
+    }
+}
+
---
+++ src/PrimeNumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static boolean isPrime(int num){
+        int count = 0;
+
+        // BRUTE FORCE : Count how many numbers divide it
+        // TC : O(N)
+//        for(int i=1; i<=num; i++){
+//            if(num % i ==0){
+//                count++;
+//            }
+//        }
+
+        // OPTIMISED APPROACH
+        // Loop till square root of the number and check the division.
+        // TC : O(sqrt(N))
+
+        for(int i=1; i<= Math.sqrt(num); i++){
+            if(num%i == 0){
+                count++;
+                if(num/i != i){
+                    count++;
+                }
+            }
+        }
+
+        if(count == 2){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number = ");
+        int num = sc.nextInt();
+        System.out.println(isPrime(num));
+    }
+}
