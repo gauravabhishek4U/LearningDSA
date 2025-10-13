1d9fe4d3b041d9c82bb2e393fa15291b14d10280
Abhishek Gaurav <gauravabhishek400@gmail.com>
2025-10-13 18:04:42 +0530

    added recursion based question

---
+++ src/recursion/Fibonacci.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class Fibonacci {
+
+    static int fibonacci(int N){
+        // Base Condition.
+        if(N <= 1){
+
+            return N;
+        }
+
+        // Problem broken down into 2 functional calls
+        // and their results combined and returned.
+        int last = fibonacci(N-1);
+        int slast = fibonacci(N-2);
+
+        return last + slast;
+    }
+
+    public static void main(String[] args) {
+        int N = 4;
+        System.out.println(fibonacci(N));
+    }
+}
